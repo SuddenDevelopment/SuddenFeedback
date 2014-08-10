f51f9ff85ee042d98eed68a008231019116aa66b
Anthony Aragues <anthony.aragues@gmail.com>
2014-08-09 22:39:09 -0400

    Local Mem storage!
    items stored locally, sorted and decides if it’s high enough to be sent
    to the UI.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var arrTerms=["Department of Homeland Security","Federal Emergency Management Agency","Coast Guard","Customs and Border Protection","Border Patrol","Secret Service","National Operations Center","Homeland Defense","Immigration Customs Enforcement","Agent","Task Force","Central Intelligence Agency","CIA","Fusion Center","Drug Enforcement Agency","DEA","Secure Border Initiative","Federal Bureau of Investigation","FBI","Alcohol Tobacco and Firearms","ATF","U.S. Citizenship and Immigration Services","Federal Air Marshal Service","Transportation Security Administration","TSA","Air Marshal","Federal Aviation Administration","FAA","National Guard","Red Cross","United Nations","Assassination","Attack","Domestic security","Drill","Exercise","Cops","Law enforcement","Authorities","Disaster assistance","Disaster management","DNDO (Domestic Nuclear Detection Office","National preparedness","Mitigation","Prevention","Response","Recovery","Dirty bomb","Domestic nuclear detection","Emergency management","Emergency response","First responder","Homeland security","Maritime domain awareness","MDA","National preparedness initiative","Militia Shooting","Shots fired","Evacuation","Deaths","Hostage","Explosion","explosive","Police","Disaster medical assistance team ","DMAT","Organized crime","Gangs","National security","State of emergency","Security","Breach","Threat","Standoff","SWAT","Screening","Lockdown","Bomb squad ","bomb threat","Crash","Looting","Riot","Emergency","Landing","Pipe bomb","Incident","Facility","Hazmat","Nuclear","Chemical spill","Suspicious package","suspicious device","Toxic","National laboratory","Nuclear facility","Nuclear threat","Cloud","Plume","Radiation","Radioactive","Leak","Biological infection","Chemical","Chemical burn","Biological","Epidemic","Hazardous","Hazardous material incident","Industrial spill","Infection","Powder","Gas","Spillover","Anthrax","Blister agent","Chemical agent","Exposure","Burn","Nerve agent","Ricin","Sarin","North Korea","Outbreak","Contamination","Exposure","Virus","Evacuation","Bacteria","Recall","Ebola","Food Poisoning","Foot and Mouth","H5N1","Avian","Flu","Salmonella","Small Pox","Plague","Human to human","Human to Animal","Influenza","Center for Disease Control","CDC","Drug Administration","FDA","Public Health","Toxic Agro","Terror Tuberculosis","Agriculture","Listeria","Symptoms","Mutation","Resistant","Antiviral","Wave","Pandemic","Infection","Water borne","air borne","Sick","Swine","Pork","Strain","Quarantine","H1N1","Vaccine","Tamiflu","Norvo Virus","Epidemic","World Health Organization","Viral Hemorrhagic Fever","E. Coli","Infrastructure security","Airport","CIKR","AMTRAK","Collapse","Computer infrastructure","Communications infrastructure","Telecommunications","Critical infrastructure","National infrastructure","Metro","WMATA","Airplane","Chemical fire","Subway","BART","MARTA","Port Authority","NBIC","Transportation security","Grid","Power","Smart","Body scanner","Electric","Failure or outage","Black out","Brown out","Port","Dock","Bridge","Cancelled","Delays","Service disruption","Power lines","Drug cartel","Violence","Gang","Drug","Narcotics","Cocaine","Marijuana","Heroin","Border","Mexico","Cartel","Southwest","Juarez","Sinaloa","Tijuana","Torreon","Yuma","Tucson","Decapitated","U.S. Consulate","Consular","El Paso","Fort Hancock","San Diego","Ciudad Juarez","Nogales","Sonora","Colombia","Mara salvatrucha","MS13 or MS-13","Drug war","Mexican army","Methamphetamine","Cartel de Golfo","Gulf Cartel","La Familia","Reynosa","Nuevo Leon","Narcos","Los Zetas","Shootout","Execution","Gunfight","Trafficking","Kidnap","Calderon","Reyosa","Bust","Tamaulipas","Meth Lab","Drug trade","Illegal immigrants","Smuggling","Matamoros","Michoacana","Guzman","Arellano-Felix","Beltran-Leyva","Barrio Azteca","Artistic Assassins","Mexicles","New Federation","Terrorism","Al Qaeda","Terror","Attack","Iraq","Afghanistan","Iran","Pakistan","Agro","Environmental terrorist","Eco terrorism","Conventional weapon","Target","Weapons grade","Dirty bomb","Enriched","Nuclear","Chemical weapon","Biological weapon","Ammonium nitrate","Improvised explosive device","IED","Abu Sayyaf","Hamas","FARC","IRA","ETA","Basque Separatists","Hezbollah","Tamil Tigers","PLF","PLO","Car bomb","Jihad","Taliban","Weapons cache","Suicide bomber","Suicide attack","Suspicious substance","AQAP","AQIM","TTP ","Yemen","Pirates","Extremism","Somalia","Nigeria","Radicals","Al-Shabaab","Home grown","Plot","Nationalist","Recruitment","Fundamentalism","Islamist","Emergency","Hurricane","Tornado","Twister","Tsunami","Earthquake","Tremor","Flood","Storm","Crest","Temblor","Extreme weather","Forest fire","Brush fire","Ice","Stranded/Stuck","Help","Hail","Wildfire","Tsunami Warning Center","Magnitude","Avalanche","Typhoon","Shelter-in-place","Disaster","Snow","Blizzard","Sleet","Mud slide or Mudslide","Erosion","Power outage","Brown out","Warning","Watch","Lightening","Aid","Relief","Closure","Interstate","Burst","Emergency Broadcast System","Cyber security","Botnet","DDOS","Denial of service","Malware","Virus","Trojan","Keylogger","Cyber Command","2600","Spammer","Phishing","Rootkit","Phreaking","Cain and abel","Brute forcing","Mysql injection","Cyber attack","Cyber terror","Hacker","China","Conficker","Worm","Scammers","Social media"];
+function getIndex(arr,key,value){ for(i=0; i<arr.length;i++){ if(arr[i][key]==value){ delete arr; return i; } } } //shim function to get the index by a key/value for when the index reported by angular doesnt match
 
 /*
  * GET home page.
@@ -102,21 +103,47 @@ exports.index = function (req, res) {
                     //loop through the column level term groups used
 
                     //go through columns in order, col order matters for sorting, some items will pass through and add to multiple aolumns, default is to stop when a col is found
+                    var intColIndex = false;
                     for(i=0;i<objReport.columns.length;i++){
                         if(objReport.columns[i].show.toLowerCase()=='notes' && objItem.analysis.filtered){ arrItems.push({column:objReport.columns[i].id,typ:'Msg',text:'filtered: '+objItem.analysis.filtered }); }
                         else if(objReport.columns[i].analysis=='sentiment=positive' && !objItem.analysis.filtered && objItem.analysis.sentiment > 0){ objItem.column=objReport.columns[i].id; }
                         else if(objReport.columns[i].analysis=='sentiment=negative' && !objItem.analysis.filtered && objItem.analysis.sentiment < 0){ objItem.column=objReport.columns[i].id; }
                         else if(objReport.columns[i].analysis=='sentiment=neutral' && !objItem.analysis.filtered && objItem.analysis.sentiment == 0){ objItem.column=objReport.columns[i].id; }
-                        else if(objReport.columns[i].show=='ColumnTitle' && objItem.text.toLowerCase().indexOf(objReport.columns[i].label.toLowerCase())!= -1){ 
-                            if(objItem.analysis.filtered){ arrItems.push({column:objReport.columns[i].id,typ:'Tag',text:'filtered: '+objItem.analysis.filtered }); }else{objItem.column=objReport.columns[i].id;}
+                        else if(objReport.columns[i].show=='ColumnTitle'){ 
+                            var strNeedle = objReport.columns[i].label.toLowerCase();
+                            if(objItem.text.toLowerCase().indexOf(strNeedle)!= -1){objItem.column=objReport.columns[i].id;}
+                            //else if(_.find(objReport.columns[i].hashtags,{ 'text':strNeedle })){ objItem.column=objReport.columns[i].id; }
                         }
+                        if(objItem.column && objItem.analysis.filtered){ arrItems.push({column:objItem.column,typ:'Tag',text:'filtered: '+objItem.analysis.filtered }); } //add a per colum tag for filtered items
+                        if(objItem.column){ intColIndex=i;}
+
                     }
                  //END COLUMN SORTING\\
                 //####################\\
-                    //objItem.user = null; //kep the browser from needing to store all this
+                //__________________________________\\
+                //----====|| STORE LOCALLY ||====----\\
+                    var torfRT = false;
+                    if(objItem.column){
+                        _.forEach(objReport.columns[intColIndex]['items'],function(objI){ 
+                            if(objI.text==objItem.text){
+                                if(objItem.priority < 2 || objItem.priority <= objI.priority){ objI.priority++;} //cumulative priority
+                                else{objI.priority = objItem.priority;} //replacing priority
+                                torfRT = true;
+                            }         
+                        });
+                        if(torfRT === false){ 
+                            objReport.columns[intColIndex]['items'].unshift(objItem); //add
+                            objReport.columns[intColIndex]['items'] = fnSortArr(objReport.columns[intColIndex]['items'],objReport.columns[intColIndex].sort); //sort the column
+                            if(getIndex(objReport.columns[intColIndex]['items'],'id',objItem.id)>objReport.columns[intColIndex].limit){ torfSend=false; }//make sure it's high enough sort order to sed to browser
+                        }
+                    }else{
+                        console.log('column: '+objItem.column+"\n"+objItem.text)+"\n \n"; //when this is triggered no column was assigned, if it happens too often something is wrong
+                    }
+                //END LOCAL STORAGE\\
+                //##################\\
 
                     //SEND IT TO THE BrowSER WEBSOCKET
-                    if(objItem.column>0){
+                    if(torfSend && objItem.column>0){
                         arrItems.push(objItem);
                         //add stats
                         arrItems.push({column:objItem.column,typ:'User',text:objItem.user.screen_name});
@@ -124,13 +151,15 @@ exports.index = function (req, res) {
                         for(var i=0;i<objItem.entities.symbols.length;i++){ arrItems.push({column:objItem.column,typ:'Symbol',text:objItem.entities.symbols[i].text.toLowerCase() }); }
                         for(var i=0;i<objItem.entities.user_mentions.length;i++){ arrItems.push({column:objItem.column,typ:'Mention',text:objItem.entities.user_mentions[i].screen_name.toLowerCase() }); }
                         for(var i=0;i<objItem.entities.hashtags.length;i++){ arrItems.push({column:objItem.column,typ:'Tag',text:objItem.entities.hashtags[i].text.toLowerCase() }); }
-                    }
+                    }else{ }
                     if(arrItems.length > 0){io.sockets.emit('newItems', arrItems);}
                 }
             });
         }); //end stream
     }
 }
+
+var fnSortArr = function(arrItems,strProp){ return _.sortBy(arrItems, function (obj) { return obj[strProp]; }); }
 
 /* TO BE PULLED INTO OTHER FILES / MODULES  */
 var fnAllTerms = function(arrNeedles,strHaystack){
